67f46f81dde5af91146e9de9ead583c80d36f7d6
VladimirSergeevichFedorov <viky2010@rambler.ru>
2022-03-13 16:58:32 +0400

    s

--- buildSrc/src/main/kotlin/dependenciesForModule/DatastoreDep.kt
+++ buildSrc/src/main/kotlin/dependenciesForModule/DatastoreDep.kt
@@ -2,6 +2,6 @@ package dependenciesForModule
 
 object DatastoreDep {
 
-    internal val datastore = "androidx.datastore:datastore-core:1.0.0-rc01"
-    internal val protobuf = "com.google.protobuf:protobuf-javalite:3.14.0"
+//    internal val datastore = "androidx.datastore:datastore-core:1.0.0-rc01"
+//    internal val protobuf = "com.google.protobuf:protobuf-javalite:3.14.0"
 }
